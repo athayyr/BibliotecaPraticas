4f11e23748fda03addd620c371dc01357f983f88
athay <athay@DESKTOP-EQPIM3G>
2017-03-24 19:10:29 -0300

    Criando clsse de teste Junit

---
+++ BibliotecaPraticas/test/br/com/praticas/model/dao/EnderecoDAOTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.praticas.model.dao;
+
+import br.com.praticas.model.bean.Endereco;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author VAAR
+ */
+public class EnderecoDAOTest {
+    
+    public EnderecoDAOTest() {
+    }
+
+    @Test
+    public void create() {
+        
+        Endereco endereco = new Endereco("Maria", "96", "Coremas");     
+        
+        EnderecoDAO dao = new EnderecoDAO();
+        
+        if(dao.create(endereco)){
+            System.out.println("Sucesso");
+        }else{
+            fail("ERRO");
+        }
+    }
+    
+}
