4860f6deccf9e372e3b6d8dfe217fe962985ea00
athay <athay@DESKTOP-EQPIM3G>
2017-03-24 00:53:28 -0300

    Criação da Classe Endereco em bean

---
+++ BibliotecaPraticas/src/br/com/praticas/model/bean/Endereco.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.praticas.model.bean;
+
+/**
+ *
+ * @author VAAR
+ */
+public class Endereco {
+    private int id;
+    private String rua;
+    private String numero;
+    private String cidade;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return this.rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+}
