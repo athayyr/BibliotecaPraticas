24706e608021fb06cb3510be370000a84c294876
athay <athay@DESKTOP-EQPIM3G>
2017-03-24 00:55:04 -0300

    Criação da Classe Pessoa em bean

---
+++ BibliotecaPraticas/src/br/com/praticas/model/bean/Pessoa.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.praticas.model.bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author VAAR
+ */
+public class Pessoa {
+    
+    private int id;
+    private String nome;
+    private Date nascimento;
+    private Endereco endereco;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return this.nascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.nascimento = dataNascimento;
+    }
+
+    public Endereco getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+}
