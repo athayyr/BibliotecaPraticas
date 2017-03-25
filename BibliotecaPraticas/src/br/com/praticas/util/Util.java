dd0d49e2e46e0f9fcdfc932f1caeccbfc6ccd374
athay <athay@DESKTOP-EQPIM3G>
2017-03-25 00:51:50 -0300

    Conversoes

--- BibliotecaPraticas/src/br/com/praticas/util/Util.java
+++ BibliotecaPraticas/src/br/com/praticas/util/Util.java
@@ -3,13 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.com.praticas.util;
+
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author VAAR
  */
 public class Util {
+
+    public static Date stringParaDate(String data) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            Date date = sdf.parse(data);
+            
+            return date;
+        } catch (ParseException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 
 }
