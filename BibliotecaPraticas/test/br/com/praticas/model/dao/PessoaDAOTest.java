1e47bb91b9038a644d14b7b2c393b9ac5d194d27
athay <athay@DESKTOP-EQPIM3G>
2017-03-26 16:14:43 -0300

    Teste de atualização

--- BibliotecaPraticas/test/br/com/praticas/model/dao/PessoaDAOTest.java
+++ BibliotecaPraticas/test/br/com/praticas/model/dao/PessoaDAOTest.java
@@ -40,6 +40,8 @@ public class PessoaDAOTest {
         
         pessoa = pessoaDAO.search(7);
         
+        pessoa.setNome("teste atualização");
+        
         pessoaDAO.update(pessoa);
     }
     
