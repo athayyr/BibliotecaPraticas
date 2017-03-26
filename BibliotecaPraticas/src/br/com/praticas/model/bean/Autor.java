bd3333a545a70f25b6e47f64d458b939f1ed5976
JoséHigor <JosHigor@JH>
2017-03-26 13:44:18 -0300

    olha aê

---
+++ BibliotecaPraticas/src/br/com/praticas/model/bean/Autor.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.praticas.model.bean;
+
+/**
+ *
+ * @author JoséHigor
+ */
+public class Autor {
+    int id;
+    String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" + "id=" + id + ", nome=" + nome + '}';
+    }
+    
+}
