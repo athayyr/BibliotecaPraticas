95cd4fbbae886a921cfd3a0d3db3dd03222038c4
athay <athay@DESKTOP-EQPIM3G>
2017-04-02 11:32:47 -0300

    Editora

---
+++ BibliotecaPraticas/src/br/com/praticas/model/bean/Editora.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.praticas.model.bean;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Editora {
+    
+    private int id;
+    private String nome;
+
+    public Editora(){   
+    }
+    
+    public Editora(int id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
