70ecc34d7c298f4c460c5d55ec3b015bd2ab4b02
athay <athay@DESKTOP-BEASAET>
2017-04-25 12:39:47 -0300

    DAOFACtory com pessoaDAO, autorDAO e enderecoDAO

--- BibliotecaPraticas/src/br/com/praticas/factory/DAOFactory.java
+++ BibliotecaPraticas/src/br/com/praticas/factory/DAOFactory.java
@@ -5,10 +5,28 @@
  */
 package br.com.praticas.factory;
 
+import br.com.praticas.model.dao.AutorDAO;
+import br.com.praticas.model.dao.EnderecoDAO;
+import br.com.praticas.model.dao.IAutorDAO;
+import br.com.praticas.model.dao.IEnderecoDAO;
+import br.com.praticas.model.dao.IPessoaDAO;
+import br.com.praticas.model.dao.PessoaDAO;
+
 /**
  *
  * @author VAAR
  */
 public class DAOFactory {
     
+    public static IPessoaDAO createPessoaDAO(){
+        return new PessoaDAO();
+    }
+    
+    public static IAutorDAO createAutorDAO(){
+        return new AutorDAO();
+    }
+    
+    public static IEnderecoDAO createEnderecoDAO(){
+        return new EnderecoDAO();
+    }
 }
