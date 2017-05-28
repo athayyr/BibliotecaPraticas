58654430ab2054c63d2bba3374afcab9ca56c55c
Renan Maia <Renan Maia@RenanMaia-PC>
2017-05-28 19:05:58 -0300

    Criado

---
+++ BibliotecaPraticas/src/br/com/praticas/model/bean/Aluno.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.praticas.model.bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author José Higor
+ */
+public class Aluno {
+    private int id;
+    private int matricula;
+    private String nome;
+    private Date nascimento;
+    private Endereco endereco;
+    private String curso;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "id=" + id + ", matricula=" + matricula + ", nome=" + nome + ", nascimento=" + nascimento + ", endereco=" + endereco + ", curso=" + curso + '}';
+    }
+    
+    
+        
+}
