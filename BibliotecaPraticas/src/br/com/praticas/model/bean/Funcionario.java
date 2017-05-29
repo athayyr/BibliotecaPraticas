45f12de136a80e6f4707c36a5a77310168add1f3
Renan Maia <Renan Maia@RenanMaia-PC>
2017-05-28 21:08:51 -0300

    Atualizado

---
+++ BibliotecaPraticas/src/br/com/praticas/model/bean/Funcionario.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.praticas.model.bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author José Higor
+ */
+public class Funcionario {
+    private int id;
+    private String nome;
+    private Date nascimento;
+    private Endereco endereco;
+    private String cargo;
+    private String login;
+    private String senha;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+       public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" + "id=" + id + ", nome=" + nome + ", nascimento=" + nascimento + ", endereco=" + endereco + ", cargo=" + cargo + '}';
+    }
+    
+    
+}
