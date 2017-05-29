8beb14ad74ea584423240ec196cb52119443f72b
Renan Maia <Renan Maia@RenanMaia-PC>
2017-05-28 21:09:26 -0300

    Atualizado

---
+++ BibliotecaPraticas/src/br/com/praticas/model/dao/FuncionarioDAO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.praticas.model.dao;
+
+import br.com.praticas.interfaces.IFuncionarioDao;
+import br.com.praticas.model.bean.Funcionario;
+import java.util.List;
+
+/**
+ *
+ * @author José Higor
+ */
+public class FuncionarioDAO implements IFuncionarioDao{
+
+    @Override
+    public boolean create(Funcionario funcionario) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(Funcionario funcionario) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Funcionario> list() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Funcionario search(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Funcionario funcionario) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
